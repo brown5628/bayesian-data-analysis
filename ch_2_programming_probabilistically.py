577c63d7b0ec38c4798da222bd43d65e8524b4b9
brown5628 <brown5628@gmail.com>
2020-01-21 07:24:31 -0500

    Continue Ch 2

--- ch_2_programming_probabilistically.py
+++ ch_2_programming_probabilistically.py
@@ -30,3 +30,174 @@ az.summary(trace)
 az.plot_posterior(trace)
 
 # %%
+az.plot_posterior(trace, rope=[.45, .55])
+
+# %%
+az.plot_posterior(trace, ref_val=.5)
+
+# %%
+grid = np.linspace(0, 1, 200)
+θ_pos = trace['θ']
+lossf_a = [np.mean(abs(i - θ_pos)) for i in grid]
+lossf_b = [np.mean((i - θ_pos)**2) for i in grid]
+
+for lossf, c in zip([lossf_a, lossf_b], ['C0', 'C1']):
+    mini = np.argmin(lossf)
+    plt.plot(grid, lossf, c)
+    plt.annotate('{:.2f}'.format(grid[mini]),
+                 (grid[mini], lossf[mini] + 0.03), color=c)
+    plt.yticks([])
+    plt.xlabel(r'$\hat \theta$')
+
+plt.show()
+
+# %%
+lossf = []
+for i in grid:
+    if i < .5:
+        f = np.mean(np.pi * θ_pos / np.abs(i - θ_pos))
+    else:
+        f = np.mean(1 / (i - θ_pos))
+    lossf.append(f)
+
+mini = np.argmin(lossf) 
+plt.plot(grid, lossf)
+plt.plot(grid[mini], lossf[mini], 'o')
+plt.annotate('{:.2f}'.format(grid[mini]),
+             (grid[mini] + 0.01, lossf[mini] + 0.1))
+plt.yticks([])
+plt.xlabel(r'$\hat \theta$')
+plt.show()
+
+# %%
+data = np.loadtxt('/home/brown5628/projects/bayesian-data-analysis/data/chemical_shifts.csv')
+az.plot_kde(data, rug=True)
+plt.yticks([0], alpha=0)
+
+# %%
+with pm.Model() as model_g: 
+    μ = pm.Uniform('μ', lower = 40, upper = 70)
+    σ = pm.HalfNormal('σ', sd = 10)
+    y = pm.Normal('y', mu = μ, sd = σ, observed = data)
+    trace_g = pm.sample(1000)
+
+az.plot_trace(trace_g)
+
+# %%
+az.plot_joint(trace_g, kind='kde', fill_last=False)
+
+# %%
+az.summary(trace_g)
+
+# %%
+y_pred_g = pm.sample_posterior_predictive(trace_g, 100, model_g)
+
+# %%
+data_ppc = az.from_pymc3(trace=trace_g, posterior_predictive=y_pred_g)
+ax = az.plot_ppc(data_ppc, figsize=(12,6), mean=False)
+ax[0].legend(fontsize=15)
+
+# %%
+np.mean(stats.t(loc=0, scale=1, df=100).rvs(100))
+
+# %%
+plt.figure(figsize=(10, 6))
+x_values = np.linspace(-10, 10, 500)
+for df in [1, 2, 30]:
+    distri = stats.t(df)
+    x_pdf = distri.pdf(x_values)
+    plt.plot(x_values, x_pdf, label=fr'$\nu = {df}$', lw=3)
+
+x_pdf = stats.norm.pdf(x_values)
+plt.plot(x_values, x_pdf, 'k--', label=r'$\nu = \infty$')
+plt.xlabel('x')
+plt.yticks([])
+plt.legend()
+plt.xlim(-5, 5)
+
+# %%
+with pm.Model() as model_t:
+    μ = pm.Uniform('μ', 40, 75)
+    σ = pm.HalfNormal('σ', sd = 10)
+    ν = pm.Exponential('ν', 1/30)
+    y = pm.StudentT('y', mu=μ, sd=σ, nu=ν, observed=data)
+    trace_t = pm.sample(1000)
+
+az.plot_trace(trace_t)
+
+# %%
+az.summary(trace_t)
+
+
+# %%
+y_ppc_t = pm.sample_posterior_predictive(
+    trace_t, 100, model_t, random_seed=123)
+y_pred_t = az.from_pymc3(trace=trace_t, posterior_predictive=y_ppc_t)
+az.plot_ppc(y_pred_t, figsize=(12, 6), mean=False)
+ax[0].legend(fontsize=15)
+plt.xlim(40, 70)
+
+# %%
+tips = pd.read_csv('/home/brown5628/projects/bayesian-data-analysis/data/tips.csv')
+tips.tail()
+
+# %%
+sns.violinplot(x='day', y='tip', data=tips)
+
+# %%
+tip = tips['tip'].values
+idx = pd.Categorical(tips['day'],
+                     categories=['Thur', 'Fri', 'Sat', 'Sun']).codes
+groups = len(np.unique(idx))
+
+# %%
+with pm.Model() as comparing_groups:
+    μ = pm.Normal('μ', mu=0, sd=10, shape=groups)
+    σ = pm.HalfNormal('σ', sd = 10, shape=groups)
+
+    y = pm.Normal('y', mu=μ[idx], sd=σ[idx], observed=tip)
+
+    trace_cg = pm.sample(5000)
+
+az.plot_trace(trace_cg)
+
+# %%
+dist = stats.norm()
+
+_, ax = plt.subplots(3, 2, figsize=(14, 8), constrained_layout=True)
+
+comparisons = [(i, j) for i in range(4) for j in range(i+1, 4)]
+pos = [(k, l) for k in range(3) for l in (0, 1)]
+
+for (i, j), (k, l) in zip(comparisons, pos):
+    means_diff = trace_cg['μ'][:, i] - trace_cg['μ'][:, j]
+    d_cohen = (means_diff / np.sqrt((trace_cg['σ'][:, i]**2 + trace_cg['σ'][:, j]**2) / 2)).mean()
+    ps = dist.cdf(d_cohen/(2**0.5))
+    az.plot_posterior(means_diff, ref_val=0, ax=ax[k, l])
+    ax[k, l].set_title(f'$\mu_{i}-\mu_{j}$')
+    ax[k, l].plot(
+        0, label=f"Cohen's d = {d_cohen:.2f}\nProb sup = {ps:.2f}", alpha=0)
+    ax[k, l].legend()
+
+# %%
+N_samples = [30, 30, 30]
+G_samples = [18, 18, 18]
+
+group_idx = np.repeat(np.arange(len(N_samples)), N_samples)
+data = []
+for i in range(0, len(N_samples)):
+    data.extend(np.repeat([1, 0], [G_samples[i], N_samples[i]-G_samples[i]]))
+
+
+# %%
+with pm.Model() as model_h:
+    μ = pm.Beta('μ', 1., 1.)
+    κ = pm.HalfNormal('κ', 10)
+
+    θ = pm.Beta('θ', alpha=μ*κ, beta=(1.0-μ)*κ, shape=len(N_samples))
+    y = pm.Bernoulli('y', p=θ[group_idx], observed=data)
+
+    trace_h = pm.sample(2000)
+az.plot_trace(trace_h) 
+
+# %%
