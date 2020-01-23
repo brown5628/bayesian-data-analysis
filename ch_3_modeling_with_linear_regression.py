d0f1b4439729588e5df11fdec57d3c10f7e48ea5
brown5628 <brown5628@gmail.com>
2020-01-23 07:40:36 -0500

    continue ch 3

--- ch_3_modeling_with_linear_regression.py
+++ ch_3_modeling_with_linear_regression.py
@@ -155,4 +155,174 @@ y_3 = ans[ans.group == 'III']['y'].values
 x_3 = x_3 - x_3.mean()
 
 # %%
-_, ax = plt.
+_, ax = plt.subplots(1, 2, figsize=(10,5))
+beta_c, alpha_c = stats.linregress(x_3, y_3)[:2]
+ax[0].plot(x_3, (alpha_c + beta_c * x_3), 'k', label=f'y ={alpha_c:.2f} + {beta_c:.2f} * x')
+ax[0].plot(x_3, y_3, 'C0o')
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('y', rotation=0)
+ax[0].legend(loc=0)
+az.plot_kde(y_3, ax=ax[1], rug=True)
+ax[1].set_xlabel('y')
+ax[1].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+# %%
+with pm.Model() as model_t:
+    α = pm.Normal('α', mu=y_3.mean(), sd=1)
+    β = pm.Normal('β', mu=0, sd=1)
+    ϵ = pm.HalfNormal('ϵ', 5)
+    ν_ = pm.Exponential('ν_', 1/29)
+    ν = pm.Deterministic('ν', ν_ + 1)
+
+    y_pred = pm.StudentT('y_pred', mu=α + β * x_3, sd=ϵ, nu=ν, observed=y_3)
+    trace_t = pm.sample(20000)
+
+# %%
+beta_c, alpha_c = stats.linregress(x_3, y_3)[:2]
+
+plt.plot(x_3, (alpha_c + beta_c * x_3), 'k', label='non-robust', alpha=.5) 
+plt.plot(x_3, y_3, 'C0o')
+alpha_m = trace_t['α'].mean()
+beta_m = trace_t['β'].mean()
+plt.plot(x_3, alpha_m + beta_m *x_3, c='k', label='robust')
+
+plt.xlabel('x')
+plt.ylabel('y', rotation = 0)
+plt.legend(loc=2)
+plt.tight_layout()
+plt.show()
+
+# %%
+az.summary(trace_t)
+
+# %%
+ppc = pm.sample_posterior_predictive(trace_t, samples = 200, model = model_t, random_seed=2)
+
+data_ppc = az.from_pymc3(trace=trace_t, posterior_predictive=ppc)
+
+ax = az.plot_ppc(data_ppc, figsize=(12,6), mean=True)
+plt.xlim(0, 12)
+plt.show()
+
+# %%
+N = 20
+M = 8
+idx = np.repeat(range(M-1), N)
+idx = np.append(idx, 7)
+np.random.seed(314)
+
+alpha_real = np.random.normal(2.5, .5, size=M)
+beta_real = np.random.beta(6, 1, size=M)
+eps_real = np.random.normal(0, .5, size=len(idx))
+
+y_m = np.zeros(len(idx))
+x_m = np.random.normal(10, 1, len(idx))
+y_m = alpha_real[idx] + beta_real[idx] * x_m + eps_real
+
+_, ax = plt.subplots(2, 4, figsize=(10, 5), sharex= True, sharey=True)
+ax = np.ravel(ax)
+j, k = 0, N 
+for i in range(M):
+    ax[i].scatter(x_m[j:k], y_m[j:k])
+    ax[i].set_xlabel('x_{i}')
+    ax[i].set_ylabel('y_{i}', rotation=0, labelpad=15)
+    ax[i].set_xlim(6, 15)
+    ax[i].set_ylim(7, 17)
+
+    j += N 
+    k += N
+plt.tight_layout() 
+plt.show()
+
+# %%
+x_centered = x_m - x_m.mean() 
+
+
+# %%
+with pm.Model() as unpooled_model:
+    α_tmp = pm.Normal('α_tmp', mu = 0, sd=10, shape=M)
+    β = pm.Normal('β', mu=0, sd=10, shape=M)
+    ϵ = pm.HalfCauchy('ϵ', 5)
+    ν = pm.Exponential('ν', 1/30)
+
+    y_pred = pm.StudentT('y_pred', mu=α_tmp[idx] + β[idx] * x_centered, sd=ϵ, nu=ν, observed=y_m)
+
+    α = pm.Deterministic('α', α_tmp - β * x_m.mean())
+
+    trace_up = pm.sample(2000)
+
+# %%
+az.plot_forest(trace_up, var_names=['α', 'β'], combined=True)
+
+# %%
+with pm.Model() as hierarchical_model:
+    # hyper-priors
+    α_μ_tmp = pm.Normal('α_μ_tmp', mu=0, sd=10)
+    α_σ_tmp = pm.HalfNormal('α_σ_tmp', 10)
+    β_μ = pm.Normal('β_μ', mu=0, sd=10)
+    β_σ = pm.HalfNormal('β_σ', sd=10)
+
+    # priors
+    α_tmp = pm.Normal('α_tmp', mu=α_μ_tmp, sd=α_σ_tmp, shape=M)
+    β = pm.Normal('β', mu=β_μ, sd=β_σ, shape=M)
+    ϵ = pm.HalfCauchy('ϵ', 5)
+    ν = pm.Exponential('ν', 1/30)
+
+    y_pred = pm.StudentT('y_pred', mu=α_tmp[idx] + β[idx] * x_centered, sd=ϵ, nu=ν, observed=y_m)
+
+    α = pm.Deterministic('α', α_tmp - β * x_m.mean())
+    α_μ = pm.Deterministic('α_μ', α_μ_tmp - β_μ * x_m.mean())
+    α_σ = pm.Deterministic('α_sd', α_σ_tmp - β_μ * x_m.mean())
+
+    trace_hm = pm.sample(1000)
+
+# %%
+az.plot_forest(trace_hm, var_names=['α', 'β'], combined=True)
+
+# %%
+_, ax = plt.subplots(2, 4, figsize=(10, 5), sharex=True, sharey=True,
+                     constrained_layout=True)
+ax = np.ravel(ax)
+j, k = 0, N
+x_range = np.linspace(x_m.min(), x_m.max(), 10)
+for i in range(M):
+    ax[i].scatter(x_m[j:k], y_m[j:k])
+    ax[i].set_xlabel(f'x_{i}')
+    ax[i].set_ylabel(f'y_{i}', labelpad=17, rotation=0)
+    alpha_m = trace_hm['α'][:, i].mean()
+    beta_m = trace_hm['β'][:, i].mean()
+    ax[i].plot(x_range, alpha_m + beta_m * x_range, c='k',
+               label=f'y = {alpha_m:.2f} + {beta_m:.2f} * x')
+    plt.xlim(x_m.min()-1, x_m.max()+1)
+    plt.ylim(y_m.min()-1, y_m.max()+1)
+    j += N
+    k += N
+plt.show()
+
+# %%
+x_2 = ans[ans.group == 'II']['x'].values
+y_2 = ans[ans.group == 'II']['y'].values
+x_2 = x_2 - x_2.mean() 
+
+plt.scatter(x_2, y_2)
+plt.xlabel('x')
+plt.ylabel('y', rotation = 0)
+
+# %%
+with pm.Model() as model_poly:
+    α = pm.Normal('α', mu=y_2.mean(), sd=1)
+    β1 = pm.Normal('β1', mu=0, sd=1)
+    β2 = pm.Normal('β2', mu=0, sd=1)
+    ϵ = pm.HalfCauchy('ϵ', 5)
+
+    mu = α + β1 * x_2 + β2 * x_2**2
+
+    y_pred = pm.Normal('y_pred', mu=mu, sd=ϵ, observed=y_2) 
+
+    trace_poly = pm.sample(2000)
+
+# %%
+x_p = np.linspace(-6, 6) 
+y_p = trace_polu['α'].mean() + trace_poly['β1'].mean() * x_p + trace_poly['']
