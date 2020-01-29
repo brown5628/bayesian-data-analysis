af4afde7a12f44176baa5fb0365ce5246058f0d5
brown5628 <brown5628@gmail.com>
2020-01-29 07:39:11 -0500

    Complete ch 4

--- ch_4_generalizing_linear_models.py
+++ ch_4_generalizing_linear_models.py
@@ -114,5 +114,216 @@ df = az.summary(trace_1)
 df
 
 # %%
+corr = iris[iris['species'] != 'virginica'].corr() 
+mask = np.tri(*corr.shape).T 
+sns.heatmap(corr.abs(), mask=mask, annot=True, cmap='viridis')
+plt.show()
 
 # %%
+df = iris.query("species == ('setosa', 'versicolor')")
+df = df[45:]
+y_3 = pd.Categorical(df['species']).codes
+x_n = ['sepal_length', 'sepal_width']
+x_3 = df[x_n].values
+
+# %%
+with pm.Model() as model_3: 
+    alpha = pm.Normal('alpha', mu=0, sd=10)
+    beta = pm.Normal('beta', mu=0, sd=2, shape=len(x_n))
+
+    mu = alpha + pm.math.dot(x_3, beta)
+    theta = 1 / (1 + pm.math.exp(-mu))
+    bd = pm.Deterministic('bd', -alpha/beta[1] - beta[0]/beta[1] * x_3[:,0])
+
+    y1 = pm.Bernoulli('y1', p=theta, observed=y_3)
+
+    trace_3 = pm.sample(1000)
+
+# %%
+idx = np.argsort(x_3[:,0])
+bd = trace_3['bd'].mean(0)[idx]
+plt.scatter(x_3[:,0], x_3[:,1], c= [f'C{x}' for x in y_3])
+plt.plot(x_3[:,0][idx], bd, color='k')
+
+az.plot_hpd(x_3[:,0], trace_3['bd'], color='k')
+
+plt.xlabel(x_n[0])
+plt.ylabel(x_n[1])
+
+# %%
+iris = sns.load_dataset('iris')
+y_s = pd.Categorical(iris['species']).codes
+x_n = iris.columns[:-1]
+x_s = iris[x_n].values
+x_s = (x_s-x_s.mean(axis=0)) / x_s.std(axis=0)
+
+
+# %%
+with pm.Model() as model_s:
+    α = pm.Normal('α', mu=0, sd=5, shape=3)
+    β = pm.Normal('β', mu=0, sd=5, shape=(4,3))
+    μ = pm.Deterministic('μ', α + pm.math.dot(x_s, β))
+    θ = tt.nnet.softmax(μ)
+    yl = pm.Categorical('yl', p=θ, observed=y_s)
+    trace_s = pm.sample(2000)
+
+# %%
+data_pred = trace_s['mu'].mean(0)
+y_pred = [np.exp(point)/np.sum(np.exp(point), axis=0) for point in data_pred]
+f'{np.sum(y_s == np.argmax(y_pred, axis=1)) / len(y_s):.2f}'
+
+# %%
+with pm.Model() as model_sf:
+    α = pm.Normal('α', mu=0, sd=2, shape=2)
+    β = pm.Normal('β', mu=0, sd=2, shape=(4,2))
+    α_f = tt.concatenate([[0] ,α])
+    β_f = tt.concatenate([np.zeros((4,1)) , β], axis=1)
+    μ = α_f + pm.math.dot(x_s, β_f)
+    θ = tt.nnet.softmax(μ)
+    yl = pm.Categorical('yl', p=θ, observed=y_s)
+    trace_sf = pm.sample(1000)
+
+# %%
+with pm.Model() as lda:
+    mean = pm.Normal('mu', mu=0, sd=10, shape=2)
+    sd = pm.HalfNormal('sd', 10)
+    setosa = pm.Normal('setosa', mu = mu[0], sd = sd, observed=x_0[:50])
+    versicolor = pm.Normal('versicolor', mu=mu[1], sd=sd, observed=x_0[50:])
+    bd = pm.Deterministic('bd', (mu[0] + mu[1]) / 2)
+    trace_lda = pm.sample(1000)
+
+# %%
+plt.axvline(trace_lda['bd'].mean(), ymax=1, color='C1')
+bd_hpd = az.hpd(trace_lda['bd'])
+plt.fill_betweenx([0, 1], bd_hpd[0], bd_hpd[1], color='C1', alpha=0.5)
+
+plt.plot(x_0, np.random.normal(y_0, 0.02), '.', color='k')
+plt.ylabel('θ', rotation=0)
+plt.xlabel('sepal_length')
+
+# %% 
+az.summary(trace_lda)
+
+# %% 
+mu_params = [.5, 1.5, 3, 8]
+x = np.arrange(0, max(mu_params) * 3)
+for mu in mu_params:
+    y = stats.poisson(mu).pmf(x)
+    plt.plot(x, y, 'o-', label=f'mu = {mu:3.1f}')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+
+# %%
+n = 100
+θ_real = 2.5
+ψ = 0.1
+
+# Simulate some data
+counts = np.array([(np.random.random() > (1-ψ)) * 
+                   np.random.poisson(θ_real) for i in range(n)])
+
+# %%
+with pm.Model() as ZIP:
+    ψ = pm.Beta('ψ', 1, 1)
+    θ = pm.Gamma('θ', 2, 0.1)
+    y = pm.ZeroInflatedPoisson('y', ψ, θ,
+                               observed=counts)
+    trace = pm.sample(1000)
+
+# %%
+az.plot_trace(trace)
+#plt.savefig('B11197_04_11.png', dpi=300)
+
+#%%
+fish_data = pd.read_csv('/home/brown5628/projects/bayesian-data-analysis/data/fish.csv')
+
+# %%
+with pm.Model() as ZIP_reg:
+    ψ = pm.Beta('ψ', 1, 1)
+    α = pm.Normal('α', 0, 10)
+    β = pm.Normal('β', 0, 10, shape=2)
+    θ = pm.math.exp(α + β[0] * fish_data['child'] + β[1] * fish_data['camper'])
+    yl = pm.ZeroInflatedPoisson('yl', ψ, θ, observed=fish_data['count'])
+    trace_ZIP_reg = pm.sample(1000)
+az.plot_trace(trace_ZIP_reg)
+
+# %%
+az.summary(trace_ZIP_reg) 
+
+# %%
+children = [0, 1, 2, 3, 4]
+fish_count_pred_0 = []
+fish_count_pred_1 = []
+for n in children:
+    without_camper = trace_ZIP_reg['α'] + trace_ZIP_reg['β'][:,0] * n
+    with_camper = without_camper + trace_ZIP_reg['β'][:,1]
+    fish_count_pred_0.append(np.exp(without_camper))
+    fish_count_pred_1.append(np.exp(with_camper))
+    
+    
+plt.plot(children, fish_count_pred_0, 'C0.', alpha=0.01)
+plt.plot(children, fish_count_pred_1, 'C1.', alpha=0.01)
+
+plt.xticks(children);
+plt.xlabel('Number of children')
+plt.ylabel('Fish caught')
+plt.plot([], 'C0o', label='without camper')
+plt.plot([], 'C1o', label='with camper')
+plt.legend()
+plt.show()
+
+# %%
+
+iris = sns.load_dataset("iris") 
+df = iris.query("species == ('setosa', 'versicolor')") 
+y_0 = pd.Categorical(df['species']).codes 
+x_n = 'sepal_length'  
+x_0 = df[x_n].values 
+y_0 = np.concatenate((y_0, np.ones(6, dtype=int))) 
+x_0 = np.concatenate((x_0, [4.2, 4.5, 4.0, 4.3, 4.2, 4.4])) 
+x_c = x_0 - x_0.mean() 
+plt.plot(x_c, y_0, 'o', color='k')
+
+# %%
+with pm.Model() as model_rlg:
+    α = pm.Normal('α', mu=0, sd=10)
+    β = pm.Normal('β', mu=0, sd=10)
+    
+    μ = α + x_c *  β  
+    θ = pm.Deterministic('θ', pm.math.sigmoid(μ))
+    bd = pm.Deterministic('bd', -α/β)
+    
+    π = pm.Beta('π', 1., 1.) 
+    p = π * 0.5 + (1 - π) * θ 
+    
+    yl = pm.Bernoulli('yl', p=p, observed=y_0)
+
+    trace_rlg = pm.sample(1000)
+
+# %%
+az.plot_trace(trace_rlg, varnames)
+
+# %%
+varnames = ['α', 'β', 'bd']
+az.summary(trace_rlg, varnames)
+
+# %%
+theta = trace_rlg['θ'].mean(axis=0)
+idx = np.argsort(x_c)
+plt.plot(x_c[idx], theta[idx], color='C2', lw=3);
+plt.vlines(trace_rlg['bd'].mean(), 0, 1, color='k')
+bd_hpd = az.hpd(trace_rlg['bd'])
+
+plt.fill_betweenx([0, 1], bd_hpd[0], bd_hpd[1], color='k', alpha=0.5)
+
+plt.scatter(x_c, np.random.normal(y_0, 0.02), marker='.', color=[f'C{x}' for x in y_0])
+theta_hpd = az.hpd(trace_rlg['θ'])[idx]
+plt.fill_between(x_c[idx], theta_hpd[:,0], theta_hpd[:,1], color='C2', alpha=0.5)
+
+plt.xlabel(x_n)
+plt.ylabel('θ', rotation=0)
+# use original scale for xticks
+locs, _ = plt.xticks() 
+plt.xticks(locs, np.round(locs + x_0.mean(), 1))
+
