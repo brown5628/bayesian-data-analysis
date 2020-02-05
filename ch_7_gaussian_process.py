5f58bc33af2850be4861024ce1a4d84a925931b8
brown5628 <brown5628@gmail.com>
2020-02-05 07:29:07 -0500

    Continue ch 7

--- ch_7_gaussian_process.py
+++ ch_7_gaussian_process.py
@@ -54,3 +54,341 @@ ax[1].set_yticks(range(len(data)))
 ax[1].xaxis.tick_top()
 
 # %%
+np.random.seed(24)
+test_points = np.linspace(0, 10, 200)
+fig, ax = plt.subplots(2, 2, figsize=(12, 6), sharex=True, sharey=True, constrained_layout=True)
+ax = np.ravel(ax)
+
+for idx, ℓ in enumerate((.2, 1, 2, 10)):
+    cov = exp_quad_kernel(test_points, test_points, ℓ)
+    ax[idx].plot(test_points, stats.multivariate_normal.rvs(cov=cov, size=2).T)
+    ax[idx].set_title(f'ℓ={ℓ}')
+fig.text(.51, -.03, 'x', fontsize=16)
+fig.text(-.03, .5, 'f(x)', fontsize=16)
+
+# %%
+np.random.seed(42)
+x = np.random.uniform(0, 10, size=15)
+y = np.random.normal(np.sin(x), .1)
+plt.plot(x, y, 'o')
+true_x = np.linspace(0,10, 200)
+plt.plot(true_x, np.sin(true_x), 'k--')
+plt.xlabel('x')
+plt.ylabel('f(x)', rotation=0)
+
+# %%
+X = x[:, None]
+
+with pm.Model() as model_reg:
+    ℓ = pm.Gamma('ℓ', 2, .5)
+
+    cov = pm.gp.cov.ExpQuad(1, ls=ℓ)
+
+    gp = pm.gp.Marginal(cov_func=cov)
+
+    ϵ = pm.HalfNormal('ϵ', 25)
+
+    y_pred = gp.marginal_likelihood('y_pred', X=X, y=y, noise=ϵ)
+
+    trace_reg = pm.sample(2000)
+# %%
+az.plot_trace(trace_reg)
+
+# %%
+X_new = np.linspace(np.floor(x.min()), np.ceil(x.max()), 100)[:, None]
+
+with model_reg:
+    f_pred = gp.conditional('f_pred', X_new)
+
+
+# %%
+with model_reg:
+    pred_samples = pm.sample_posterior_predictive(trace_reg, vars=[f_pred], samples = 82)
+
+# %%
+_, ax = plt.subplots(figsize=(12,5))
+ax.plot(X_new, pred_samples['f_pred'].T, 'C1-', alpha=.3)
+ax.plot(X, y, 'ko')
+ax.set_xlabel('X')
+
+# %%
+_, ax = plt.subplots(figsize=(12,5))
+
+pm.gp.util.plot_gp_dist(ax, pred_samples['f_pred'], X_new, palette='viridis', plot_samples=False);
+
+ax.plot(X, y, 'ko')
+ax.set_xlabel('x')
+ax.set_ylabel('f(x)', rotation=0, labelpad=15)
+
+# %%
+_, ax = plt.subplots(figsize=(12,5))
+
+# predict
+point = {'ℓ': trace_reg['ℓ'].mean(), 'ϵ': trace_reg['ϵ'].mean()}
+mu, var = gp.predict(X_new, point=point, diag=True)
+sd = var**0.5
+
+# plot mean and 1σ and 2σ intervals
+ax.plot(X_new, mu, 'C1')
+ax.fill_between(X_new.flatten(),
+                 mu - sd, mu + sd,
+                 color="C1",
+                 alpha=0.3)
+
+ax.fill_between(X_new.flatten(),
+                 mu - 2*sd, mu + 2*sd,
+                 color="C1",
+                 alpha=0.3)
+
+ax.plot(X, y, 'ko')
+ax.set_xlabel('X')
+
+# %%
+islands_dist = pd.read_csv('/home/brown5628/projects/bayesian-data-analysis/data/islands_dist.csv', sep=',', index_col=0)
+islands_dist.round(1)
+
+# %%
+islands = pd.read_csv('/home/brown5628/projects/bayesian-data-analysis/data/islands.csv', sep=',')
+islands.head().round(1)
+
+islands_dist_sqr = islands_dist.values**2
+culture_labels = islands.culture.values
+index = islands.index.values
+log_pop = islands.logpop
+total_tools = islands.total_tools
+x_data = [islands.lat.values[:, None], islands.lon.values[:, None]]
+
+# %%
+with pm.Model() as model_islands:
+    η = pm.HalfCauchy('η', 1)
+    ℓ = pm.HalfCauchy('ℓ', 1)
+    
+    cov = η * pm.gp.cov.ExpQuad(1, ls=ℓ)
+    gp = pm.gp.Latent(cov_func=cov)
+    f = gp.prior('f', X=islands_dist_sqr)
+
+    α = pm.Normal('α', 0, 10)
+    β = pm.Normal('β', 0, 1)
+    μ = pm.math.exp(α + f[index] + β * log_pop)
+    tt_pred = pm.Poisson('tt_pred', μ, observed=total_tools)
+    trace_islands = pm.sample(1000, tune=1000)
+
+
+# %%
+trace_η = trace_islands['η']
+trace_ℓ = trace_islands['ℓ']
+
+_, ax = plt.subplots(1, 1, figsize=(8, 5))
+xrange = np.linspace(0, islands_dist.values.max(), 100)
+
+ax.plot(xrange, np.median(trace_η) *
+        np.exp(-np.median(trace_ℓ) * xrange**2), lw=3)
+
+ax.plot(xrange, (trace_η[::20][:, None] * np.exp(- trace_ℓ[::20][:, None] * xrange**2)).T,
+        'C0', alpha=.1)
+
+ax.set_ylim(0, 1)
+ax.set_xlabel('distance (thousand kilometers)')
+ax.set_ylabel('covariance')
+
+# %%
+# compute posterior median covariance among societies
+Σ = np.median(trace_η) * (np.exp(-np.median(trace_ℓ) * islands_dist_sqr))
+# convert to correlation matrix
+Σ_post = np.diag(np.diag(Σ)**-0.5)
+ρ = Σ_post @  Σ @ Σ_post
+ρ = pd.DataFrame(ρ, index=islands_dist.columns, columns=islands_dist.columns)
+ρ.round(2)
+
+# %% 
+logpop = np.copy(log_pop)
+logpop /= logpop.max()
+psize = np.exp(logpop*5.5)
+log_pop_seq = np.linspace(6, 14, 100)
+lambda_post = np.exp(trace_islands['α'][:, None] +
+                     trace_islands['β'][:, None] * log_pop_seq)
+
+_, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+ax[0].scatter(islands.lon2, islands.lat, psize, zorder=3)
+ax[1].scatter(islands.logpop, islands.total_tools, psize, zorder=3)
+
+for i, itext in enumerate(culture_labels):
+    ax[0].text(islands.lon2[i]+1, islands.lat[i]+1, itext)
+    ax[1].text(islands.logpop[i]+.1, islands.total_tools[i]-2.5, itext)
+
+
+ax[1].plot(log_pop_seq, np.median(lambda_post, axis=0), 'k--')
+
+az.plot_hpd(log_pop_seq, lambda_post, fill_kwargs={'alpha':0},
+            plot_kwargs={'color':'k', 'ls':'--', 'alpha':1})
+
+
+for i in range(10):
+    for j in np.arange(i+1, 10):
+        ax[0].plot((islands.lon2[i], islands.lon2[j]),
+                   (islands.lat[i], islands.lat[j]), 'C1-',
+                   alpha=ρ.iloc[i, j]**2, lw=4)
+        ax[1].plot((islands.logpop[i], islands.logpop[j]),
+                   (islands.total_tools[i], islands.total_tools[j]), 'C1-',
+                   alpha=ρ.iloc[i, j]**2, lw=4)
+ax[0].set_xlabel('longitude')
+ax[0].set_ylabel('latitude')
+
+
+ax[1].set_xlabel('log-population')
+ax[1].set_ylabel('total tools')
+ax[1].set_xlim(6.8, 12.8)
+ax[1].set_ylim(10, 73)
+
+# %%
+iris = pd.read_csv('/home/brown5628/projects/bayesian-data-analysis/data/iris.csv')
+iris.head()
+
+# %%
+df = iris.query("species == ('setosa', 'versicolor')")
+y = pd.Categorical(df['species']).codes
+x_1 = df['sepal_length'].values
+X_1 = x_1[:, None]
+
+
+# %%
+with pm.Model() as model_iris:
+    #ℓ = pm.HalfCauchy("ℓ", 1)
+    ℓ = pm.Gamma('ℓ', 2, 0.5)
+    cov = pm.gp.cov.ExpQuad(1, ℓ)
+    gp = pm.gp.Latent(cov_func=cov)
+    f = gp.prior("f", X=X_1)
+    # logistic inverse link function and Bernoulli likelihood
+    y_ = pm.Bernoulli("y", p=pm.math.sigmoid(f), observed=y)
+    trace_iris = pm.sample(1000, chains=1, compute_convergence_checks=False)
+
+# %%
+X_new = np.linspace(np.floor(x_1.min()), np.ceil(x_1.max()), 200)[:, None]
+
+with model_iris:
+    f_pred = gp.conditional('f_pred', X_new)
+    pred_samples = pm.sample_posterior_predictive(
+        trace_iris, vars=[f_pred], samples=1000)
+
+#%%
+def find_midpoint(array1, array2, value):
+    """
+    This should be a proper docstring :-)
+    """
+    array1 = np.asarray(array1)
+    idx0 = np.argsort(np.abs(array1 - value))[0]
+    idx1 = idx0 - 1 if array1[idx0] > value else idx0 + 1
+    if idx1 == len(array1):
+        idx1 -= 1
+    return (array2[idx0] + array2[idx1]) / 2
+
+# %%
+_, ax = plt.subplots(figsize=(10, 6))
+
+fp = logistic(pred_samples['f_pred'])
+fp_mean = np.mean(fp, 0)
+
+ax.plot(X_new[:, 0], fp_mean)
+# plot the data (with some jitter) and the true latent function
+ax.scatter(x_1, np.random.normal(y, 0.02),
+           marker='.', color=[f'C{x}' for x in y])
+
+az.plot_hpd(X_new[:, 0], fp, color='C2')
+
+db = np.array([find_midpoint(f, X_new[:, 0], 0.5) for f in fp])
+db_mean = db.mean()
+db_hpd = az.hpd(db)
+ax.vlines(db_mean, 0, 1, color='k')
+ax.fill_betweenx([0, 1], db_hpd[0], db_hpd[1], color='k', alpha=0.5)
+ax.set_xlabel('sepal_length')
+ax.set_ylabel('θ', rotation=0)
+
+# %%
+with pm.Model() as model_iris2:
+    #ℓ = pm.HalfCauchy("ℓ", 1)
+    ℓ = pm.Gamma('ℓ', 2, 0.5)
+    c = pm.Normal('c', x_1.min())
+    τ = pm.HalfNormal('τ', 5)
+    cov = (pm.gp.cov.ExpQuad(1, ℓ) +
+           τ * pm.gp.cov.Linear(1, c) +
+           pm.gp.cov.WhiteNoise(1E-5))
+    gp = pm.gp.Latent(cov_func=cov)
+    f = gp.prior("f", X=X_1)
+    # logistic inverse link function and Bernoulli likelihood
+    y_ = pm.Bernoulli("y", p=pm.math.sigmoid(f), observed=y)
+    trace_iris2 = pm.sample(1000, chains=1, compute_convergence_checks=False)
+
+# %%
+with model_iris2:
+    f_pred = gp.conditional('f_pred', X_new)
+    pred_samples = pm.sample_posterior_predictive(trace_iris2,
+                                                  vars=[f_pred],
+                                                  samples=1000)
+
+# %%
+_, ax = plt.subplots(figsize=(10,6))
+
+fp = logistic(pred_samples['f_pred'])
+fp_mean = np.mean(fp, 0)
+
+ax.scatter(x_1, np.random.normal(y, 0.02), marker='.', color=[f'C{ci}' for ci in y])
+
+db = np.array([find_midpoint(f, X_new[:,0], 0.5) for f in fp])
+db_mean = db.mean()
+db_hpd = az.hpd(db)
+ax.vlines(db_mean, 0, 1, color='k')
+ax.fill_betweenx([0, 1], db_hpd[0], db_hpd[1], color='k', alpha=0.5)
+
+ax.plot(X_new[:,0], fp_mean, 'C2', lw=3)
+az.plot_hpd(X_new[:,0], fp, color='C2')
+
+ax.set_xlabel('sepal_length')
+ax.set_ylabel('θ', rotation=0)
+#%%
+df_sf = pd.read_csv('../data/space_flu.csv')
+age = df_sf.age.values[:, None]
+space_flu = df_sf.space_flu
+
+ax = df_sf.plot.scatter('age', 'space_flu', figsize=(8, 5))
+ax.set_yticks([0, 1])
+ax.set_yticklabels(['healthy', 'sick'])
+# %%
+with pm.Model() as model_space_flu:
+    ℓ = pm.HalfCauchy('ℓ', 1)
+    cov = pm.gp.cov.ExpQuad(1, ℓ) + pm.gp.cov.WhiteNoise(1E-5)
+    gp = pm.gp.Latent(cov_func=cov)
+    f = gp.prior('f', X=age)
+    y_ = pm.Bernoulli('y', p=pm.math.sigmoid(f), observed=space_flu)
+    trace_space_flu = pm.sample(
+        1000, chains=1, compute_convergence_checks=False)
+
+# %%
+X_new = np.linspace(0, 80, 200)[:, None]
+
+with model_space_flu:
+    f_pred = gp.conditional('f_pred', X_new)
+    pred_samples = pm.sample_posterior_predictive(trace_space_flu,
+                                                  vars=[f_pred],
+                                                  samples=1000)
+# %%
+_, ax = plt.subplots(figsize=(10, 6))
+
+fp = logistic(pred_samples['f_pred'])
+fp_mean = np.nanmean(fp, 0)
+
+ax.scatter(age, np.random.normal(space_flu, 0.02),
+           marker='.', color=[f'C{ci}' for ci in space_flu])
+
+ax.plot(X_new[:, 0], fp_mean, 'C2', lw=3)
+
+az.plot_hpd(X_new[:, 0], fp, color='C2')
+ax.set_yticks([0, 1])
+ax.set_yticklabels(['healthy', 'sick'])
+ax.set_xlabel('age')
+
+# %%
+coal_df = pd.read_csv('/home/brown5628/projects/bayesian-data-analysis/data/coal.csv', header=None)
+coal_df.head()
+
+# %%
