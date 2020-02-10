2821c4e2beb82e33b58dc53ea892c689b978abba
brown5628 <brown5628@gmail.com>
2020-02-10 07:20:29 -0500

    complete book

--- ch_8_inference_engines.py
+++ ch_8_inference_engines.py
@@ -57,3 +57,108 @@ plt.xlabel('θ', fontsize=14)
 plt.yticks([])
 
 # %%
+N = 10000
+
+x, y = np.random.uniform(-1, 1, size=(2, N))
+inside = (x**2 + y**2) <= 1
+pi = inside.sum() * 4/N 
+error = abs((pi - np.pi) / pi) * 100
+
+outside = np.invert(inside)
+
+plt.figure(figsize=(8,8))
+plt.plot(x[inside], y[inside], 'b.')
+plt.plot(x[outside], y[outside], 'r.')
+plt.plot(0, 0, label=f'n*= {pi:4.3f}\nerror = {error:4.3f}', alpha =0)
+plt.axis('square')
+plt.xticks([])
+plt.yticks([])
+plt.legend(loc=1, frameon=True, framealpha=.9)
+
+
+
+# %%
+def metropolis(func, draws=10000):
+    """A very simple Metropolis implementation"""
+    trace = np.zeros(draws)
+    old_x = .5
+    old_prob = func.pdf(old_x)
+
+    delta = np.random.normal(0, .5, draws)
+    for i in range(draws):
+        new_x = old_x + delta[i]
+        new_prob = func.pdf(new_x)
+        acceptance = new_prob / old_prob 
+        if acceptance >= np.random.random():
+            trace[i] = new_x 
+            old_x = new_x 
+            old_prob = new_prob 
+        else:
+            trace[i] = old_x 
+    
+    return trace 
+
+
+
+# %%
+np.random.seed(3) 
+func = stats.beta(2, 5)
+trace = metropolis(func=func)
+x = np.linspace(.01, .99, 100)
+y = func.pdf(x)
+plt.xlim(0,1)
+plt.plot(x, y, 'C1-', lw=3, label='True distribution')
+plt.hist(trace[trace > 0], bins=25, density=True, label='Estimated distribution')
+plt.xlabel('x')
+plt.ylabel('pdf(x)')
+plt.yticks([])
+plt.legend()
+
+# %%
+with pm.Model() as centered_model:
+    a = pm.HalfNormal('a', 10)
+    b = pm.Normal('b', 0, a, shape=10)
+    trace_cm = pm.sample(2000, random_seed=7)
+
+with pm.Model() as non_centered_model:
+    a = pm.HalfNormal('a', 10)
+
+    b_shift = pm.Normal('b_offset', mu=0, sd=1, shape=10)
+    b = pm.Deterministic('b', 0 + b_shift * a)
+    trace_ncm = pm.sample(2000, random_seed=7)
+
+
+# %%
+az.plot_trace(trace_cm, var_names=['a'], divergences='top')
+
+
+# %%
+az.plot_trace(trace_ncm, var_names=['a'])
+
+# %%
+az.plot_forest(trace_cm, var_names=['a'], r_hat=True, ess=True)
+
+# %%
+summaries = pd.concat([az.summary(trace_cm, var_names=['a']), az.summary(trace_ncm, var_names=['a'])])
+summaries.index = ['centered', 'non_centered']
+summaries
+
+# %%
+az.plot_autocorr(trace_cm, var_names=['a'])
+
+# %%
+az.plot_autocorr(trace_ncm, var_names=['a'])
+
+# %%
+_, ax = plt.subplots(1, 2, sharey=True, sharex=True, figsize=(10, 5), constrained_layout=True)
+
+for idx, tr in enumerate([trace_cm, trace_ncm]):
+    az.plot_pair(tr, var_names=['b', 'a'], coords={'b_dim_0':[0]}, kind='scatter',
+                 divergences=True, contour=False, divergences_kwargs={'color':'C1'},
+                 ax=ax[idx])
+    ax[idx].set_title(['centered', 'non-centered'][idx])
+
+# %%
+az.plot_parallel(trace_cm)
+
+# %%
